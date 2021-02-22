fc5aabf06fe8e42fde90428575691a03363633be
Jungelr <viktor.panovski@yahoo.com>
2021-02-23 00:05:33 +0100

    AfterMergeConflicts

--- src/main/java/com/hvt/booking_lux/web/AccountController.java
+++ src/main/java/com/hvt/booking_lux/web/AccountController.java
@@ -1,10 +1,11 @@
 package com.hvt.booking_lux.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
 public class AccountController {
-
 }
